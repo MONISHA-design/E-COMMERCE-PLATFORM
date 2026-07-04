9666a61badea2c0967f86821a1afdf726bc04460
To-Do List App <monishaverma208@gmail.com>
2026-07-04 20:11:30 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 // Middleware to protect routes and verify the JWT token
 export const protect = async (req, res, next) => {
